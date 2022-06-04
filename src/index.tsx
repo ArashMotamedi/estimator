6b1c548df9b58e32294c35618884981be277b68d
Arash Motamedi <arash.motamedi@gmail.com>
2022-06-04 10:16:48 -0700

    ignore errors

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
-import { Calculator } from './Calculator';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
